6d700e21ddf0a278a84f34fcd5f946766306fab5
brianngeywo <brianngeywo254@gmail.com>
2020-05-17 11:59:33 +0300

    add users from ui

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root to: "pages#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :rentals
+  resources :users, except: [:new]
+  get 'signup', to: 'users#new' 
 end
