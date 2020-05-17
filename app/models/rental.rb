ddd818d133695d735d6b7bddfe6475508b0a8714
brianngeywo <brianngeywo254@gmail.com>
2020-05-17 09:58:21 +0300

    add user data to rantals listing

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,7 +1,7 @@
 class Rental < ActiveRecord::Base
     belongs_to :user
     validates :title, presence: true, length: {minimum:3, maximum:50}
-    validates :description, presence: true, length: {minimum: 10, maximum: 300}
+    validates :description, presence: true, length: {minimum: 10, maximum: 10000}
     validates :user_id, presence: true, uniqueness: { case_sensitive: false }
     
        
