6a479363d615eb9ee5b9b699cacfcf83fe60171e
brianngeywo <brianngeywo254@gmail.com>
2020-05-17 15:43:56 +0300

    users index and links

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,39 +1,43 @@
 class UsersController < ApplicationController
 
-    def new
-        @user = User.new
-    end
-    def edit
-        @user = User.find(params[:id])
-    end
-    
-    def create
-        @user = User.new(user_params)
-        if @user.save
-          flash[:success] = "Welcome to the alpha rent"
-          redirect_to rentals_url
-        else
-          flash[:error] = "Something went wrong"
-          render 'new'
-        end
-    end
-    def show
-        @user = User.find(params[:id])
-    end
-    def update
-        @user = User.find(params[:id])
-        if @user.update_attributes(user_params)
-          flash[:success] = "User was successfully updated"
-          redirect_to @user
-        else
-          flash[:error] = "Something went wrong"
-          render 'edit'
-        end
-    end
-    
-    
-    private
-    def user_params
-        params.require(:user).permit(:username, :email, :password)
-    end
+  def index
+    @users = User.all
+  end
+  
+  def new
+    @user = User.new
+  end
+  def edit
+    user = User.find(params[:id])
+  end
+  
+  def create
+      @user = User.new(user_params)
+      if @user.save
+        flash[:success] = "Welcome to the alpha rent"
+        redirect_to rentals_url
+      else
+        flash[:error] = "Something went wrong"
+        render 'new'
+      end
+  end
+  def show
+      @user = User.find(params[:id])
+  end
+  def update
+      @user = User.find(params[:id])
+      if @user.update_attributes(user_params)
+        flash[:success] = "User was successfully updated"
+        redirect_to @user
+      else
+        flash[:error] = "Something went wrong"
+        render 'edit'
+      end
+  end
+  
+  
+  private
+  def user_params
+      params.require(:user).permit(:username, :email, :password)
+  end
 end
