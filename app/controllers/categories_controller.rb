4ff69f72d567e670a04b48a17ba0c9d5d6eb0353
brianngeywo <brianngeywo254@gmail.com>
2020-05-19 21:05:53 +0300

    finishing touches, edit categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController
-    before_action :require_admin_user, only: [:new]
+    before_action :require_admin_user, except: [:index, :show]
     
     def index
         @categories = Category.all
@@ -24,6 +24,31 @@ class CategoriesController < ApplicationController
           render 'new'
         end
     end
+    def edit
+        @category = Category.find(params[:id])
+    end
+    def update
+        @category = Category.find(params[:id])
+        if @category.update_attributes(category_params)
+        flash[:success] = "category name was successfully updated"
+        redirect_to category_path(@category)
+        else
+        flash[:error] = "Something went wrong"
+        render 'edit'
+        end
+    end
+    def destroy
+        @category = Category.find(params[:id])
+        if @category.destroy
+            flash[:success] = 'Category was successfully deleted.'
+            redirect_to categories_path
+        else
+            flash[:error] = 'Something went wrong'
+            redirect_to category_path(@category)
+        end
+    end
+    
+     
     
     private
     def category_params
@@ -31,7 +56,7 @@ class CategoriesController < ApplicationController
     end
     def require_admin_user
         if !current_user.admin?
-            flash[:danger] = "only admin can add new categories"
+            flash[:danger] = "only admin can add or edit categories"
             redirect_to root_path
         end
       end
